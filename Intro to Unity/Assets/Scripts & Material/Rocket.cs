2e4d39a9f51039276b37761c658d8db56373385e
RhettBooher13 <rhettbooher@gmail.com>
2023-02-01 15:24:34 -0600

    Made diamonds disappear when they touch rocket.

---
+++ Intro to Unity/Assets/Scripts & Material/Rocket.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        print("Collision");
+        if (col.gameObject.tag == "Diamond") {
+            Destroy(col.gameObject);
+        }
+    }
+}
